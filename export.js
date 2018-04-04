7f4f2565a16478db009c2959af639ee9a601cd0d
Dale Nguyen <dungnq@itbox4vn.com>
2018-04-04 10:28:41 -0400

    Added ability to export sub collection #1

--- export.js
+++ export.js
@@ -1,21 +1,22 @@
-var admin = require("firebase-admin");
-var fs = require('fs');
-var serviceAccount = require("./serviceAccountKey.json");
+const admin = require("firebase-admin");
+const fs = require('fs');
+const serviceAccount = require("./serviceAccountKey.json");
 
-var collectionName = process.argv[2];
+let collectionName = process.argv[2];
+let subCollection = process.argv[3];
 
-// You should replae databaseURL with your own
+// You should replace databaseURL with your own
 admin.initializeApp({
   credential: admin.credential.cert(serviceAccount),
   databaseURL: "https://ionic-firestore-dn.firebaseio.com"
 });
 
-var db = admin.firestore();
+let db = admin.firestore();
 
-var data = {};
+let data = {};
 data[collectionName] = {};
 
-var results = db.collection(collectionName)
+let results = db.collection(collectionName)
 .get()
 .then(snapshot => {
   snapshot.forEach(doc => {
@@ -27,12 +28,35 @@ var results = db.collection(collectionName)
   console.log(error);
 })
 
-results.then(dt => {
-  // Write collection to JSON file
-  fs.writeFile("firestore-export.json", JSON.stringify(dt), function(err) {
-      if(err) {
-          return console.log(err);
-      }
-      console.log("The file was saved!");
-  });
+results.then(dt => {  
+  getSubCollection(dt).then(() => {    
+    // Write collection to JSON file
+    fs.writeFile("firestore-export.json", JSON.stringify(data), function(err) {
+        if(err) {
+            return console.log(err);
+        }
+        console.log("The file was saved!");
+    });
+  })  
 })
+
+async function getSubCollection(dt){
+  for (let [key, value] of Object.entries([dt[collectionName]][0])){
+    if(subCollection !== undefined){
+      data[collectionName][key]['subCollection'] = {};
+      await addSubCollection(key, data[collectionName][key]['subCollection']);            
+    }          
+  }  
+}
+
+function addSubCollection(key, subData){
+  return new Promise(resolve => {
+    db.collection(collectionName).doc(key).collection(subCollection).get()
+    .then(snapshot => {      
+      snapshot.forEach(subDoc => {             
+        subData[subDoc.id] =  subDoc.data();
+        resolve('Added data');                                                                 
+      })
+    })
+  })
+}
